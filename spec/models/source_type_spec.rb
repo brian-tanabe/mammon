2354315d227bfb6fa076a6a87f620d0e00bbca16
Brian Tanabe <btanabe2@gmail.com>
2020-03-21 11:38:34 -0700

    Updating source type tests

--- spec/models/source_type_spec.rb
+++ spec/models/source_type_spec.rb
@@ -4,20 +4,32 @@ RSpec.describe SourceType, type: :model do
 
 	describe 'create' do
 
-		it 'should be able to create a source type' do
-			source_type = create(:source_type)
-			source_type.save!
+		context 'when the source type is valid' do
 
-			# Verify against the persisted source type
-			expect(SourceType.find(source_type.id).name).to eq(source_type.name)
+			it 'should be able to create a source type' do
+				source_type = create(:source_type)
+
+				# Verify against the persisted source type
+				expect(SourceType.find(source_type.id).name).to eq(source_type.name)
+			end
+
 		end
 
-		it 'should not allow redundant names' do
-			source_type = create(:source_type)
-			source_type.save!
+		context 'when the source type is not valid' do
 
-			another_source_type = build(:source_type, name: source_type.name)
-			expect { another_source_type.save! }.to raise_error(ActiveRecord::RecordNotUnique)
+			it 'should not allow redundant names' do
+				source_type = create(:source_type)
+
+				another_source_type = build(:source_type, name: source_type.name)
+				expect { another_source_type.save! }.to raise_error(ActiveRecord::RecordNotUnique)
+			end
+
+			it 'should not allow source types without users' do
+				invalid_source_type = build(:source_type, user: nil)
+
+				expect { invalid_source_type.save! }.to raise_error(ActiveRecord::RecordInvalid)
+			end
+
 		end
 
 	end
